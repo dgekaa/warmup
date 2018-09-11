4bdd9560956e25eff66dcc111251e3804ec7dd7f
evgeniy <dgeka_vip@mail.ru>
2018-09-11 11:45:29 +0300

    my

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+  var fareng;
+
+  if(temperature !== undefined){
+  	fareng = (1.8*temperature)+32; 
+  }
+
+  return fareng;
 };
